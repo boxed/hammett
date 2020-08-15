442858139541b5e63f5a9158df7cb8d8c5cdb948
Anders Hovmöller <boxed@killingar.net>
2020-08-15 20:58:49 +0200

    Don't try to collect hidden files (fixes #7)

--- hammett/__init__.py
+++ hammett/__init__.py
@@ -272,7 +272,7 @@ def handle_dir(result, d):
     for root, dirs, files in os.walk(d):
         files = [
             f for f in files
-            if f.endswith('__tests.py') or (f.startswith('test_') and f.endswith('.py')) or f.endswith('_test.py')
+            if not f.startswith('.') and f.endswith('__tests.py') or (f.startswith('test_') and f.endswith('.py')) or f.endswith('_test.py')
         ]
         result.extend(join(root, x) for x in files)
 
